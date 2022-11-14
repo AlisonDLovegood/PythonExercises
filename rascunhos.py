692ab8d31ff1e102eb1382c8e668cea1e56125d0
Alison Lovegood <alisondlovegood@gmail.com>
2022-11-14 12:21:12 -0300

    good practices, snake case

---
+++ rascunhos.py
@@ -0,0 +1,16 @@
+# color = {"c1": "Red", "c2": "Green", "c3": "Orange"}
+# for value in color.values():
+#     # if value == -1:
+#     print(value)
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             x = nums[i]
+#             for j in range(len(nums)):
+#                 y = nums[j]
+#                 if (x+y) == target:
+#                     print(f"[{i},{j}]")
+#                     exit()
+
+# Solution =
