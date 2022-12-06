9327269ecb46ecf9cb1dd07c09c15a703ab67beb
Alison Lovegood <alisondlovegood@gmail.com>
2022-12-05 21:27:34 -0300

    juvenal corrected

--- BSI/PISI2/desafios/desafio01_juvenal.py
+++ BSI/PISI2/desafios/desafio01_juvenal.py
@@ -1,22 +1,21 @@
 def f(n, a, b):
     if n == 1:
         if n > a and n < b:
-            g(chamadas)
+            g(chamadas, n)
         return 1
     elif n % 2 == 0:
         if n > a and n < b:
-            g(chamadas)
+            g(chamadas, n)
         return f(n/2, a, b)
     else:
         if n > a and n < b:
-            g(chamadas)
+            g(chamadas, n)
         return f(3*n+1, a, b)
 
 
-def g(chamadas):
-    chamadas.append(1)
-    x = sum(chamadas)
-    return x
+def g(chamadas, n):
+    chamadas.append(n)
+    return chamadas
 
 
 chamadas = []
@@ -26,5 +25,6 @@ for i in range(t):
     entradas.append(list(map(int, input().split())))
 
 for i in range(len(entradas)):
+    chamadas = []
     f(entradas[i][1], entradas[i][0], entradas[i][1])
-    print(f'Caso {i+1}: {g(chamadas)-1}')
+    print(f'Caso {i+1}: {max(chamadas):.0f}')
